23b61b48021e9616bbaf584153bf22f93aad0c3a
Rachel Simonsen <rachellsimonsen@gmail.com>
2022-04-15 10:36:34 -0600

    commit

--- writeCode.js
+++ writeCode.js
@@ -39,22 +39,9 @@ console.log(hasUniqueChars('asdfghjkl;'))
 
 //3) Pangram Sentence
 
-const str = 'The quick brown fox jumps over the lazy dog';
-const isPangram = (str = '') => {
-   str = str.toLowerCase();
-   const { length } = str;
-   const alphabets = 'abcdefghijklmnopqrstuvwxyz';
-   const alphaArr = alphabets.split('');
-   for(let i = 0; i < length; i++){
-      const el = str[i];
-      const index = alphaArr.indexOf(el);
-      if(index !== -1){
-         alphaArr.splice(index, 1);
-      };
-   };
-   return !alphaArr.length;
-};
-console.log(isPangram(str));
+
+
+//
 
 
 
